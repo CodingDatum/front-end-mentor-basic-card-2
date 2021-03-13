3c6809fb56a71ba5e6ca1a3f451501985b5dfd12
CodingDatum <73291844+CodingDatum@users.noreply.github.com>
2021-03-12 23:42:57 -0500

    initial commit
    This time i compared it to the example picturr from the design guide for this challenge

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleShare(){
+    let textArea = document.querySelector(".pop-up")
+    let triangle = document.querySelector(".triangle")
+
+    textArea.classList.toggle("show-elements")
+    triangle.classList.toggle("show-elements")
+
+}
